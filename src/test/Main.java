e7678f7f127f04cd78bc762df1eb8f503c197838
CodeurEnFougere <rtiquetduriez@iut.univ-paris8.fr>
2018-05-04 16:53:28 +0200

    Update test du main

--- src/test/Main.java
+++ src/test/Main.java
@@ -3,8 +3,9 @@ package test;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		System.out.println("Update");
 	}
 
 }
