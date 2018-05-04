54d8cf28af0ba66f6373f87a5972f13d7b2d3a67
CodeurEnFougere <rtiquetduriez@iut.univ-paris8.fr>
2018-05-04 17:03:48 +0200

    blablabla

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,11 +1,11 @@
-package test;
+package game;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		
-		System.out.println("Update");
+		System.out.println("Update new");
 	}
 
 }
