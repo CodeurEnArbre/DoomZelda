f0ba8f5de88da4495a5e632f8a1aa9d6174f68a5
CodeurEnFougere <rtiquetduriez@iut.univ-paris8.fr>
2018-05-05 14:50:23 +0200

    Update world

--- src/game/controleur/world/World.java
+++ src/game/controleur/world/World.java
@@ -9,15 +9,30 @@ public class World {
 	
 	private String zoneName;
 	private Tile tiles[];
-	private ArrayList<TileEntity> tileEntitys;
+	private ArrayList<TileEntity> tileEntity;
 	private int width,height;
 	
-	public World(String zoneName, int width, int height, Tile[] tiles, ArrayList<TileEntity> tileEntitys) {
+	public World(String zoneName, int width, int height, Tile[] tiles, ArrayList<TileEntity> tileEntity) {
 		this.zoneName=zoneName;
 		this.width=width;
 		this.height=height;
 		this.tiles=tiles;
-		this.tileEntitys=tileEntitys;
+		this.tileEntity=tileEntity;
+	}
+	
+	public String getName() {
+		return this.zoneName;
+	}
+	
+	public Tile getTile(int x, int y) {
+		if(x<=this.width||y<=this.height) {
+			return tiles[x+y*this.width+1];
+		}else
+			return null;
+	}
+	
+	public ArrayList<TileEntity> getTileEntity(){
+		return this.tileEntity;
 	}
 	
 }
