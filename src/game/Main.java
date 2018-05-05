b0c5c83ae3f8bb5ca1b82687f0d4d31edd6f21b7
CodeurEnFougere <rtiquetduriez@iut.univ-paris8.fr>
2018-05-05 17:26:25 +0200

    World Loading

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,10 +1,16 @@
 package game;
 
+import java.io.File;
+
+import game.controleur.world.WorldLoader;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello WORLD!!!");
+		WorldLoader.loadWorld(new File("ressources/map/TestMap.map"));
 		
+		System.out.println("Info : "+WorldLoader.currentMap.getTile(12, 62));
 	}
 
 }
--- src/game/controleur/world/WorldLoader.java
+++ src/game/controleur/world/WorldLoader.java
@@ -1,5 +1,73 @@
 package game.controleur.world;
 
-public class WorldLoader {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import game.controleur.tile.Tile;
+import game.controleur.tile.TileEntity;
+
+public class WorldLoader {
+	
+	public static World currentMap;
+	
+	public static void loadWorld(File file) {
+		try {
+		
+			String name="";
+			int width = 0,height=0;
+			Tile[] tile = null;
+			ArrayList<TileEntity> tileEntity= new ArrayList<TileEntity>();
+		
+			
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String ligne;
+			int ligneNum=1;
+			while ((ligne = br.readLine()) != null) {
+				
+				switch(ligneNum){
+				case 1:
+					name=ligne;
+					break;
+				case 2:
+					width=Integer.parseInt(ligne);
+					break;
+				case 3:
+					height=Integer.parseInt(ligne);
+					break;
+				case 4:
+					tile=new Tile[width*height];
+					int charAt=0;
+					int tileAt=0;
+					String tileId="";
+					while(charAt<ligne.length()) {
+						if(ligne.charAt(charAt)==',') {
+							tile[tileAt]=new Tile(Integer.parseInt(tileId));
+							tileAt++;
+							tileId="";
+						}else {
+							tileId+=""+ligne.charAt(charAt);
+						}
+						
+						charAt++;
+					}
+					if(tileId!=null)
+						tile[tileAt]=new Tile(Integer.parseInt(tileId));
+					break;
+				}
+				
+				ligneNum++;
+			}
+			br.close();
+		
+			currentMap=new World(name, width, height, tile, tileEntity);
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
