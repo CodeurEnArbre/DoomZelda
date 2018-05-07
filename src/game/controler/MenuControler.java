066d689d945a6e088bcebfd82b3f15f882d8896b
CodeurEnFougere <rtiquetduriez@iut.univ-paris8.fr>
2018-05-08 01:17:34 +0200

    Replacing TilePane to Pane

--- src/game/controler/MenuControler.java
+++ src/game/controler/MenuControler.java
@@ -2,6 +2,7 @@ package game.controler;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import game.modele.utils.Direction;
 import game.modele.world.WorldLoader;
 import game.vue.EntityLivingTexture;
@@ -10,13 +11,10 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.TilePane;
 
 public class MenuControler implements Initializable{
 
@@ -24,13 +22,13 @@ public class MenuControler implements Initializable{
 	private Pane EntityPane;
 
 	@FXML
-	private TilePane TilePaneGround;
+	private Pane PaneGround;
 
 	@FXML
-	private TilePane TilePaneSolid;
+	private Pane PaneSolid;
 
 	@FXML
-	private TilePane TilePaneTop;
+	private Pane PaneTop;
 
 	private ImageView player;
 
@@ -50,7 +48,7 @@ public class MenuControler implements Initializable{
 		
 	}
 
-	private void printCalqueTile(TilePane paneTerrain,TilePane paneTile,TilePane paneTop) {
+	private void printCalqueTile(Pane paneTerrain,Pane paneTile,Pane paneTop) {
 		
 		for(int y=0;y<WorldLoader.currentMap.getHeight();y++) {
 			for(int x=0;x<WorldLoader.currentMap.getWidth();x++) {
@@ -58,21 +56,27 @@ public class MenuControler implements Initializable{
 					
 					int i = WorldLoader.currentMap.getTileTerrain(y, x).getId();
 					i = i == 0 ? 0 : i -1;
-					     						
-					paneTerrain.getChildren().add(new ImageView(SwingFXUtils.toFXImage(TileTexture.getTileTexture(i).getTexture(), null)));
-					
+					  
+					ImageView tile = new ImageView(SwingFXUtils.toFXImage(TileTexture.getTileTexture(i).getTexture(), null));
+					paneTerrain.getChildren().add(tile);
+					tile.setX(x*32);
+					tile.setY(y*32);
 					
 					i = WorldLoader.currentMap.getTile(y, x).getId();
 					i = i == 0 ? 0 : i -1;
 					
-					paneTile.getChildren().add(new ImageView(SwingFXUtils.toFXImage(TileTexture.getTileTexture(i).getTexture(), null)));
-					
+					tile = new ImageView(SwingFXUtils.toFXImage(TileTexture.getTileTexture(i).getTexture(), null));
+					paneTile.getChildren().add(tile);
+					tile.setX(x*32);
+					tile.setY(y*32);
 					
 					i = WorldLoader.currentMap.getTileTop(y, x).getId();
 					i = i == 0 ? 0 : i -1;
 					
-					paneTop.getChildren().add(new ImageView(SwingFXUtils.toFXImage(TileTexture.getTileTexture(i).getTexture(), null)));
-					
+					tile = new ImageView(SwingFXUtils.toFXImage(TileTexture.getTileTexture(i).getTexture(), null));
+					paneTop.getChildren().add(tile);
+					tile.setX(x*32);
+					tile.setY(y*32);
 			}
 		}
 	}
@@ -85,14 +89,14 @@ public class MenuControler implements Initializable{
 		
 		WorldLoader.loadWorld("TinyMap");
 
-		TilePaneGround.setMinSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
-		TilePaneGround.setMaxSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
-		TilePaneSolid.setMinSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
-		TilePaneSolid.setMaxSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
-		TilePaneTop.setMinSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
-		TilePaneTop.setMaxSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
+		PaneGround.setMinSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
+		PaneGround.setMaxSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
+		PaneSolid.setMinSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
+		PaneSolid.setMaxSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
+		PaneTop.setMinSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
+		PaneTop.setMaxSize(WorldLoader.currentMap.getWidth()*32, WorldLoader.currentMap.getHeight()*32);
 		
-		printCalqueTile(TilePaneGround,TilePaneSolid,TilePaneTop);
+		printCalqueTile(PaneGround,PaneSolid,PaneTop);
 		
 	
 		player=new ImageView();
