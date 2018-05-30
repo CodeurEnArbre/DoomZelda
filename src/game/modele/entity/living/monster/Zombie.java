b192ba3a3aa95c2940b2f3dc3b8456701c41a80e
NATHAN CARACCIOLO <ncaracciolo@iut.univ-paris8.fr>
2018-05-30 11:02:48 +0200

    Zombie

--- src/game/modele/entity/living/monster/Zombie.java
+++ src/game/modele/entity/living/monster/Zombie.java
@@ -3,31 +3,35 @@ package game.modele.entity.living.monster;
 import game.modele.entity.Entity;
 import game.modele.utils.Coordonnees;
 import game.modele.utils.Direction;
+import game.modele.utils.ActionConsumer.ConsumerAction;
 import game.modele.utils.ActionConsumer.InfiniteActionConsumer;
 import game.modele.utils.ActionConsumer.Function.FunctionIA;
 import game.modele.utils.ActionConsumer.Function.FunctionMove;
 import game.modele.utils.ActionConsumer.Function.FunctionMovement;
 
 public class Zombie extends EntityMonster{
-	
-	
 
+	ConsumerAction deplacement = new InfiniteActionConsumer(new FunctionMove());
+	ConsumerAction mouvement = new InfiniteActionConsumer(new FunctionMovement());
+	ConsumerAction ia = new InfiniteActionConsumer(new FunctionIA());
+	
+	
 	public Zombie(Coordonnees c,Direction d) {
 		super("Zombie",c,d);
 		this.speed = 0.02;
 		this.acce = 0;
 		this.baseSpeed = 0.05;
 		this.maxSpeed = 0.05;		
-		addAction(new InfiniteActionConsumer(new FunctionMove()));
-		addAction(new InfiniteActionConsumer(new FunctionIA()));
-		addAction(new InfiniteActionConsumer(new FunctionMovement()));
-		this.nbFrameAnimation = 4;
+		addAction(deplacement);
+		addAction(mouvement);
+		addAction(ia);
 	}
 
 	@Override
 	public void dispose() {
-		delAction(FunctionMove.class.getName());
-		delAction(FunctionIA.class.getName());
+		delAction(deplacement);
+		delAction(mouvement);
+		delAction(ia);
 	}
 	@Override
 	public void active(Entity e) {
