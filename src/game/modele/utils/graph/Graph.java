4e53db56d91b4497f4d6724c303d4081f79c2cac
NATHAN CARACCIOLO <ncaracciolo@iut.univ-paris8.fr>
2018-06-06 09:36:59 +0200

    ligne droite

--- src/game/modele/utils/graph/Graph.java
+++ src/game/modele/utils/graph/Graph.java
@@ -2,6 +2,7 @@ package game.modele.utils.graph;
 
 import java.awt.Point;
 import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 import game.modele.world.World;
@@ -12,10 +13,14 @@ public class Graph {
 	public static final int[] bot = new int[] {1,0};
 	public static final int[] right = new int[] {0,-1};
 	public static final int[] left = new int[] {0,1};
-	
-	public int[][] node;
-	public SimpleEntry<Integer,int[]>[][] direction;
+
+	public int[][] node;//contient les valeurs de déplacement de chaque tile
+
+
+	public SimpleEntry<Integer,int[]>[][] direction;//indique la direction à prendre calculer par dijkstra
 	public LinkedList<Point> queue;
+
+	public ArrayList<SimpleEntry<Point, int[]>> ligneDroite;
 
 	private int width;
 	private int height;
@@ -24,6 +29,7 @@ public class Graph {
 		this.width = width;
 		this.height = height;
 		node = new int[width][height];
+		ligneDroite = new ArrayList<>();
 	}
 
 	public void init() {
@@ -34,15 +40,13 @@ public class Graph {
 		}
 	}
 
-
-
 	public void Dijkstra(int x,int y){
 		direction = new SimpleEntry[width][height];
 		queue = new LinkedList<Point>();
 		push(x,y,new int[]{0,0},0);
 		queue.addLast(new Point(x,y));
 		Dijcalcul(0);	
-		
+
 	}
 	private void Dijcalcul(int min) {
 		while(queue.peek() != null) {
@@ -83,7 +87,6 @@ public class Graph {
 			queue.addLast(new Point(nx,ny));
 		}
 	}
-
 	private int getValue(int x,int y) {
 		if(x >= height || x < 0 || y >= width || y < 0) {
 			return -1;
@@ -92,4 +95,27 @@ public class Graph {
 			return (World.currentMap.getTileTerrain(x,y).speedIndice());
 		else return -1;
 	}
+
+	/*
+	Algo qui determine les différents points accessibles
+	 en ligne droite et sans obstacle par rapport aux coordonnés du joueur
+	 */
+	public void AlgoLigneDroite(int x,int y) {
+		ligneDroite.clear();
+		for(int[] vector : new int[][] {top,bot,left,right}) {
+			ligneDroite(x,y,vector);
+		}
+	}
+	private void ligneDroite(int x,int y,int[] direction) {
+		x += direction[0];
+		y += direction[1];
+		while(x >= 0 && y >= 0 && x < width && y < height && !World.currentMap.getTile(x, y).solid()) {
+			ligneDroite.add(new SimpleEntry<Point, int[]>(new Point(x,y), direction));
+			x += direction[0];
+			y += direction[1];
+		}
+	}
+
+
+
 }
